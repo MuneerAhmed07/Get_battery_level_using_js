c9f32d6cd940d9c31da9641179ec5b51febad6b6
MuneerAhmed07 <105144687+MuneerAhmed07@users.noreply.github.com>
2022-12-18 15:10:47 +0500

    get-battery-level-using-js

---
+++ script.js
@@ -0,0 +1,15 @@
+let percentage = document.querySelector(".percentage");
+let percent = document.querySelector(".percent");
+
+// Get battery
+navigator.getBattery().then(function(battery) {
+    percentage.style.width = battery.level * 100 + '%';
+    percent.innerHTML = battery.level * 100 + '%';
+});
+
+let sec = document.querySelector('.sec');
+let toggle = document.querySelector('.toggle');
+
+toggle.onclick = function() {
+    sec.classList.toggle("dark");
+}
